7c773b6a149de1e898c9010408352f100cf9c3dd
Karim797 <karimdeheya22@gmail.com>
2026-05-11 23:57:03 +0300

    Update 2_Employee_Attrition_Prediction.py

--- pages/2_Employee_Attrition_Prediction.py
+++ pages/2_Employee_Attrition_Prediction.py
@@ -4,9 +4,8 @@ st.set_page_config(page_title="Employee Attrition Predictor", page_icon="🤖", 
 
 st.markdown("""
 <style>
-
 .hero {
-    background: linear-gradient(135deg, #0f172a, #1e3a8a);
+    background: linear-gradient(135deg, #052e16, #166534);
     padding: 45px;
     border-radius: 22px;
     color: white;
@@ -20,18 +19,23 @@ st.markdown("""
 
 .hero p {
     font-size: 19px;
-    color: #e5e7eb;
+    color: #dcfce7;
 }
 
 .stButton > button {
     width: 100%;
-    background: linear-gradient(90deg, #0f172a, #2563eb);
+    background: linear-gradient(90deg, #052e16, #16a34a);
     color: white;
     border-radius: 12px;
     padding: 14px;
     font-size: 18px;
     font-weight: 700;
     border: none;
+}
+
+.stButton > button:hover {
+    background: linear-gradient(90deg, #064e3b, #22c55e);
+    color: white;
 }
 </style>
 """, unsafe_allow_html=True)
