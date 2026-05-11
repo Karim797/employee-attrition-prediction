1cfd56fa416de4ddaf1049434cbbef5864ab9f68
Karim797 <karimdeheya22@gmail.com>
2026-05-11 23:05:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Employee Attrition Prediction",
-    page_icon="📊",
-    layout="wide"
-)
-
-st.title("Employee Attrition Prediction Project")
-
-st.write("""
-Welcome to the Employee Attrition Prediction System.
-
-Use the sidebar to navigate between:
-- EDA Dashboard
-- Prediction Page
-""")
+st.switch_page("pages/1_EDA_Dashboard.py")
